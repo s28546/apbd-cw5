29320235d9384aacc8c76d79b041d4133d85daa6
Hubert Michalski <s28546@pjwstk.edu.pl>
2024-04-08 12:35:31 +0200

    'init'

--- WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e55458d7e4c5e8d1dc174a560706a14081c264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c03672412bd2c58c5e5d146b057a1f2d10b924")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
